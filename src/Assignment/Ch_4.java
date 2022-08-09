1df2fb07e41fe149f7955f25484c579282398379
CreatePath <hary0427@naver.com>
2022-08-09 16:00:26 +0900

    3차시 과제

--- src/Assignment/Ch_4.java
+++ src/Assignment/Ch_4.java
@@ -1,2 +1,40 @@
-package Assignment;public class Ch_4 {
+package Assignment;
+
+public class Ch_4 {
+    public static void main(String[] args) {
+        // 1번
+        // wife 포함 x ==> 첫번째 조건문 false
+        // once 포함 run 포함 ==> 두번째 조건문 false
+        // everywhere 포함 x ==> 세번째 조건문 true
+        // 따라서 출력 결과는 everywhere
+
+        // 2번
+        int i = 1, sum = 0;
+        while (i <= 1000) {
+            if (i % 3 == 0) sum += i;
+            ++i;
+        }
+        System.out.println(sum);
+
+        // 3번
+        for (int k = 0; k < 5; ++k) {
+            for (int j = 0; j < k + 1; ++j) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // 4번
+        for (int l = 0; l < 100; ++l) {
+            System.out.println(l+1);
+        }
+
+        // 5번
+        int score_sum = 0;
+        int[] marks = {70, 60, 55, 75, 95, 90, 80, 80, 85, 100};
+        for (int score: marks) {
+            score_sum += score;
+        }
+        System.out.println(score_sum/ marks.length);
+    }
 }
--- src/Assignment/Ch_5.java
+++ src/Assignment/Ch_5.java
@@ -1,2 +1,65 @@
-package Assignment;public class Ch_5 {
+package Assignment;
+
+// 클래스 하나. 멤버변수 2개 이상, 멤버함수 1개 이상.
+// 객체 생성 && 메소드 사용.
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class Club {
+    String clubName;
+    int members_cnt;
+    int money;
+    String room;
+    String president;
+    ArrayList<String> members;
+
+    Club(String clubName, int money, String room, String president,
+         ArrayList<String> members) {
+        this.clubName = clubName;
+        this.money = money;
+        this.room = room;
+        this.president = president;
+        this.members = members;
+        this.members_cnt = members.size();
+    }
+
+    void info() {
+        System.out.println("동아리 이름: " + this.clubName);
+        System.out.println("회장: " + this.president);
+        System.out.println("부원: " + this.members);
+        System.out.println("동방: " + this.room);
+        System.out.println("총 인원: " + this.members_cnt);
+    }
+
+    void greetingToPresident() {
+        System.out.println(this.president + "님 납시오~");
+    }
+
+    void beggingMoney() {
+        System.out.println("현재 가진 돈:" + this.money);
+        if (this.money < 20000) {
+            System.out.println("돈 좀 주시오");
+            this.money += 100;
+        } else {
+            System.out.println("조금만 더 주시오.");
+            this.money += 1;
+        }
+        System.out.println("현재 가진 돈:" + this.money);
+    }
+
+}
+
+public class Ch_5 {
+    public static void main(String[] args) {
+        ArrayList<String> members = new ArrayList<>(Arrays.asList("세종대왕", "이순신", "홍길동"));
+        int money = 12345;
+        String room = "경복궁";
+        String clubName = "조선";
+
+        Club chosun = new Club(clubName, money, room, members.get(0), members);
+        chosun.info();
+        chosun.greetingToPresident();
+        chosun.beggingMoney();
+    }
 }
